d1c7e3f8a04091e9909254be95b94dff9c682513
Alex Castillo <wikitelmex@gmail.com>
2021-10-28 11:52:37 -0600

    Update Calculator.js
    - fit the example of refactoring that Microverse brings

--- src/components/Calculator/Calculator.js
+++ src/components/Calculator/Calculator.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import './Calculator.css';
 import calculate from '../../logic/calculate';
 
-function Calculator() {
+const Calculator = () => {
   const [state, setState] = useState({
     total: '',
     next: '',
@@ -44,6 +44,6 @@ function Calculator() {
       </div>
     </div>
   );
-}
+};
 
 export default Calculator;
