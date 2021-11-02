1e81c1e49f941000352364c6569d39074f0c73af
Alex Castillo <wikitelmex@gmail.com>
2021-11-02 15:21:37 -0700

    Navbar fit to changes required
    - destroy navbar component to put directly code inside app.js to be "semantically correct"

--- src/App.js
+++ src/App.js
@@ -1,5 +1,15 @@
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+} from 'react-router-dom';
+
 import React from 'react';
-import NavMenu from './components/NavMenu/NavMenu.lazy';
+
+import CalculatorPage from './components/pages/CalculatorPage';
+import HomePage from './components/pages/HomePage';
+import QuotePage from './components/pages/QuotePage';
 
 import './App.css';
 
@@ -7,7 +17,39 @@ function App() {
   return (
 
     <div className="m-3">
-      <NavMenu />
+      <Router>
+        <div data-testid="NavMenu">
+          <nav className="navbar navbar-expand-lg navbar-light bg-warning fixed-top">
+            <div className="container-fluid">
+              <Link className="navbar-brand h1" to="/">Math Magicians</Link>
+              <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                <span className="navbar-toggler-icon" />
+              </button>
+              <div className="collapse navbar-collapse me-auto" id="navbarNavAltMarkup">
+                <div className="navbar-nav ms-auto text-light">
+                  <Link to="/" className="nav-link">Home</Link>
+                  <Link to="/Calculator" className="nav-link">Calculator</Link>
+                  <Link to="/Quote" className="nav-link">Quote</Link>
+                </div>
+              </div>
+            </div>
+          </nav>
+        </div>
+        <br />
+        <br />
+        <br />
+        <Switch>
+          <Route path="/Calculator">
+            <CalculatorPage />
+          </Route>
+          <Route path="/Quote">
+            <QuotePage />
+          </Route>
+          <Route path="/">
+            <HomePage />
+          </Route>
+        </Switch>
+      </Router>
     </div>
 
   );
