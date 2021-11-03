50cb1911e146e495053795bd14408070710a3847
Alex Castillo <wikitelmex@gmail.com>
2021-11-03 10:50:42 -0700

    aditional snapshot test
    - add APP main component snapshot test
    -fix linters

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,15 @@
 import { render, screen } from '@testing-library/react';
+import renderer from 'react-test-renderer';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const app = screen.getByTestId('App');
-  expect(app).toBeInTheDocument();
+describe('APP(main component)', () => {
+  test('App exists', () => {
+    render(<App />);
+    const app = screen.getByTestId('App');
+    expect(app).toBeInTheDocument();
+  });
+  it('it should match snapshot', () => {
+    const tree = renderer.create(<App />).toJSON();
+    expect(tree).toMatchSnapshot();
+  });
 });
